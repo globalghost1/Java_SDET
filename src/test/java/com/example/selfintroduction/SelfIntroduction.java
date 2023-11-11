d9aec81c0b5dfba94a9b63f06068980bfa06a089
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-11 11:25:32 +0100

    Introduction to Java_Selenium

---
+++ src/test/java/com/example/selfintroduction/SelfIntroduction.java
@@ -0,0 +1,27 @@
+package com.example.selfintroduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelfIntroduction {
+    public static void main(String[] args) {
+        //Invoking browser
+        //Chrome - ChromeDriver ->Methods close
+        // Firefox  - FireFoxDriver -> Methods close
+        //Safari SafariDriver -> methods close
+        //Webdriver close get
+        //WebDriver methods + class methods
+
+        // chromedriver.exe --> Chrome browser
+        //
+
+        //SeleniumManger
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+    }
+}
+
+
