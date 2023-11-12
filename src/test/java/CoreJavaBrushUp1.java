d38d022a7ffffa9b6e246053244c016647568eca
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-12 19:06:05 +0100

    Arrays_Introduction

--- src/test/java/CoreJavaBrushUp1.java
+++ src/test/java/CoreJavaBrushUp1.java
@@ -7,7 +7,22 @@ public class CoreJavaBrushUp1 {
         double dec  = 5.99;
         boolean MyCard = true;
 
-        System.out.println(myNum);
+        System.out.println(myNum + "Is the value stored in the myNum variable");
+        System.out.println(website);
+
+        //Arrays -
+        int [] arr = new int [5];
+        arr [0] = 1;
+        arr [1] = 2;
+        arr[2] = 4;
+        arr[3] = 5;
+        arr[4] = 6;
+
+        int[] arr2 = {1,2,3,4,5,6};
+
+        system.out.println(arr2[2]);
+
+
 
 
 
