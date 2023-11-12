423df1eb6103c1fbcdc7e6fc09f10fb5ebbc605c
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-12 19:25:43 +0100

    Arrays_Introduction_2

--- src/test/java/CoreJavaBrushUp1.java
+++ src/test/java/CoreJavaBrushUp1.java
@@ -18,11 +18,24 @@ public class CoreJavaBrushUp1 {
         arr[3] = 5;
         arr[4] = 6;
 
-        int[] arr2 = {1,2,3,4,5,6};
+        int[] arr2 = {1,2,3,4,5,6,7,8,9,10,22};
 
         system.out.println(arr2[2]);
 
+        //for loop arr.length - 5
+        for(int i = 0; i< arr.length; i++) //4
+        {
+            System.out.println(arr[i]);
+        }
+        for (int i=0; i<arr2.length;) {
+            System.out.println(arr2 [i]);
+        }
+        String [] name = {"rahul","shetty","selenium"};
 
+        for(int i=0; i<name.length;i++);
+        {
+            System.out.println(name[1]);
+        }
 
 
 
