1b7cd6c81bcb8337d4150feb52a7dece7be8956d
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-13 07:30:25 +0100

    Indetyfing whe Web elements with id and name locators.Example.

---
+++ src/test/java/Locators.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+    public static void main(String[]args) {
+
+        System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+        driver.findElement(By.className("signInBtn")).click();
+
+
+    }
+
+}
--- src/test/java/MethodsDemo.java
+++ src/test/java/MethodsDemo.java
@@ -20,3 +20,6 @@ public class MethodsDemo {
         return "Rahul Shetty";
     }
 }
+
+
+
