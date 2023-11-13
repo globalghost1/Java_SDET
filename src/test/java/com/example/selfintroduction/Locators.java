6ece65e085420947a173345cb73a5a3dca01d2e1
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-13 19:19:45 +0100

    Continue programing learning

--- src/test/java/com/example/selfintroduction/Locators.java
+++ src/test/java/com/example/selfintroduction/Locators.java
@@ -4,18 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Locators {
 
     public static void main(String[]args) {
 
+        //Implicit wait - 5second time
         System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("hello123");
         driver.findElement(By.className("signInBtn")).click();
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-        driver.close();
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+        driver.findElement((By.xpath("//input[@placeholder='Email']"))).sendKeys("john@rsa.com");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@gmail.com");
+
+
 
 
     }
