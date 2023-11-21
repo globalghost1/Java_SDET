2425e98066f59ed7e325b9291ac871b26c1550bb
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-21 18:16:01 +0100

    Locators_Continue

--- src/test/java/com/example/selfintroduction/Locators.java
+++ src/test/java/com/example/selfintroduction/Locators.java
@@ -8,10 +8,10 @@ import java.time.Duration;
 
 public class Locators {
 
-    public static void main(String[]args) {
+    public static void main(String[]args) throws InterruptedException {
 
         //Implicit wait - 5second time
-        System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
@@ -20,6 +20,7 @@ public class Locators {
         driver.findElement(By.className("signInBtn")).click();
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
         driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
         driver.findElement((By.xpath("//input[@placeholder='Email']"))).sendKeys("john@rsa.com");
         driver.findElement(By.cssSelector("input[@type='text' [2]]")).clear();
@@ -27,11 +28,9 @@ public class Locators {
         driver.findElement(By.xpath("//form/input[3]")).sendKeys("98643");
         driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
         System.out.println(driver.findElement(By.cssSelector("form p")).getText());
-
-
-
-
-
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshetyacademy");
+        driver.findElement(By.id("checkBox")).click();
     }
 
 }
