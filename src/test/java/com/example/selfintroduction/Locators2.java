e9780cddddfa99125fbed553c81aa2a93e5ef44a
Patryk Ksiazek <globalghost1@protonmail.com>
2023-11-23 17:37:08 +0100

    Identyfing elements by Tags elements

---
+++ src/test/java/com/example/selfintroduction/Locators2.java
@@ -0,0 +1,18 @@
+package com.example.selfintroduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Locators2 {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("signInBtn")).click();
+    }
+}
